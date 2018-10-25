0ffcad65f2552e1b53baa6cbfcc2ce3ea8587b34
WebK1tchen <skonline.design@mail.ru>
2018-10-25 21:05:06 +0300

    старт

--- routes/addUser.js
+++ routes/addUser.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var format = require('util').format;
+
+var url = 'mongodb://localhost:27017';
+
+
 
 //Process POST request
 router.post("/", function(req, res){
-    res.send(req.body.name);
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+        if (err) throw err;
+        var db = client.db('wefour_data')
+        var users = db.collection('users');
+        var user = {
+            login: req.body.login,
+            password: req.body.password
+        }
+        users.insertOne(user, function(err, result){
+            if (err) {
+                console.log(err);
+                return;
+            }
+            res.send(result.ops);
+            client.close();
+        })
+    })
 })
 
 module.exports = router;
+
+function newFunction_1(res, req) {
+    newFunction(res, req);
+}
+
+function newFunction(res, req) {
+    res.send(req.body.name);
+}
