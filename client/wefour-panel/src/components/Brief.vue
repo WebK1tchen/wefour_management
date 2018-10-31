a0978a65051676a18d2a0f831fb9b0085615e213
WebK1tchen <skonline.design@mail.ru>
2018-10-31 16:59:06 +0300

    brief added

---
+++ client/wefour-panel/src/components/Brief.vue
@@ -0,0 +1,24 @@
+<template>
+	<html>
+		<head>
+			<title>Бриф на разработку</title>
+		</head>
+		<body>
+			<v-app>
+				
+			</v-app>
+		</body>
+	</html>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
