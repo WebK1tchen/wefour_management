11798581fc9db1e449cc2e6b813fa3b07080e1f4
AUEdeveloper <bryansky.yan2012@yandex.ru>
2018-11-03 14:51:06 +0300

    The Breif is added

--- client/wefour-panel/src/router/index.js
+++ client/wefour-panel/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LoginForm from '@/components/LoginForm'
+import Brief from '@/components/Brief'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'LoginForm',
       component: LoginForm
+    },
+    {
+      path: '/brief',
+      name: 'Brief',
+      component: Brief
     }
   ]
 })
