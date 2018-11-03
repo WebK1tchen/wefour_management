c50912d090cb845287fd0c0c372aefc52e911161
WebK1tchen <skonline.design@mail.ru>
2018-11-03 19:35:42 +0300

    123

--- client/wefour-panel/src/router/index.js
+++ client/wefour-panel/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LoginForm from '@/components/LoginForm'
+import StartPage from '@/components/StartPage'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'LoginForm',
       component: LoginForm
+    },
+    {
+      path: '/panel',
+      name: 'StartPage',
+      component: StartPage
     }
   ]
 })
